466de4f0c001edbfff927acafee1478a55705bb1
VladlenNurgalier <vlad.nurgalier@mail.ru>
2023-01-12 18:27:43 +0300

    ProjectEight

---
+++ TestProject/TestProject.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+using namespace std;
+
+class IBicycle
+{
+public:
+    void virtual TwistTheWheel() = 0;
+    void virtual Ride() = 0;
+};
+
+////////////////////////////////////////////
+
+class SimpleBicycle : public IBicycle
+{
+public:
+    void TwistTheWheel() override
+    {
+        cout << "TwistTheWheel" << endl;
+    }
+
+    void Ride() override
+    {
+        cout << "Ride" << endl;
+    }
+};
+
+class SportBibcycle : public IBicycle
+{
+public:
+    void TwistTheWheel() override
+    {
+        cout << "SportBibcycle TwistTheWheel" << endl;
+    }
+
+    void Ride() override
+    {
+        cout << "SportBibcycle Ride" << endl;
+    }
+};
+
+////////////////////////////////////////////
+
+class Human
+{
+public:
+    void RideOn(IBicycle & value)
+    {
+        cout << "Ride" << endl;
+        value.TwistTheWheel();
+        cout << endl << "Edem" << endl;
+        value.Ride();
+    }
+};
+
+int main()
+{
+    Human human;
+    SimpleBicycle s1;
+    SportBibcycle s2;
+
+    human.RideOn(s1);
+
+    cout << endl  << "-------------------------" << endl;
+
+    human.RideOn(s2);
+
+
+    return 0;
+}
---
+++ TestProject2/TestProject2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+
+class Component
+{
+public:
+    string CompanyName;
+
+    Component(string CompanyName)
+    {
+        cout << "Constructor Component" << endl;
+        this->CompanyName = CompanyName;
+    }
+};
+
+/////////////////////////////////////////////////////
+
+class GPU : public Component
+{
+public:
+    GPU(string CompanyName) : Component(CompanyName)
+    {
+        cout << "Constructor GPU" << endl;
+    }
+};
+
+class Memory : public Component
+{
+public:
+    Memory(string CompanyName) : Component(CompanyName)
+    {
+        cout << "Constructor Memory" << endl;
+    }
+};
+
+class GraphicCard : public GPU, public Memory
+{
+public:
+    GraphicCard(string GPUCompanyName, string MemoryCompanyName): GPU(GPUCompanyName), Memory(MemoryCompanyName)
+    {
+        cout << "Constructor GraphicCard" << endl;
+    }
+};
+
+
+int main()
+{
+    GraphicCard gc("NVIDIA", "Samsung");
+    //Memory m("M");
+    //GPU g("G");
+    //Component c("Iron");
+
+
+
+
+    return 0;
+}
